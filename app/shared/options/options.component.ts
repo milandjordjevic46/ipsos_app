54d1c5c4f3b7ce45448833ffbe9af311153e2cb3
Milan <mdordevicsd@gmail.com>
2020-01-16 17:23:42 +0100

    android v.3 release

--- app/shared/options/options.component.ts
+++ app/shared/options/options.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import * as appSettings from "application-settings";
 import { Router } from "@angular/router";
 import { Page } from "tns-core-modules/ui/page/page";
+import { MainService } from "../../main.service";
 
 @Component({
   selector: "app-options",
@@ -9,10 +10,19 @@ import { Page } from "tns-core-modules/ui/page/page";
   styleUrls: ["./options.component.css"]
 })
 export class OptionsComponent implements OnInit {
+  support_mails: any;
+  support: string;
   constructor(private router: Router, private page: Page) {
     page.actionBarHidden = true;
+    this.support_mails = {
+      sr: "istrazivanje@ipsos.rs",
+      hr: "online.hr@mg.ipsosadria.com",
+      sl: "online.si@mg.ipsosadria.com"
+    };
   }
-  ngOnInit() {}
+  ngOnInit() {
+    this.support = this.support_mails[appSettings.getString("survey_lng")];
+  }
 
   logOut() {
     appSettings.remove("emailIpsosApp");
